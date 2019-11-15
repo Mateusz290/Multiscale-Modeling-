be6b54fb14164653eea9b8fe9b16ca0986c19246
Mateusz290 <mateuszdabrowski02@gmail.com>
2019-11-15 19:37:45 +0100

    Continue create basic image

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+
 public class Controller {
+
+    Button button;
+
+    AnchorPane output;
+
+    public void createImage() {
+
+        output.getChildren().add(Main.createSimpleImage());
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,71 +4,66 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import sample.model.Grain;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main extends Application {
 
-    private Grain grains[][];
+    private static Grain grains[][];
     private Stage stage;
     private Controller controller;
+    private static int size = (int) 1.0 * 700 / 500;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         stage = primaryStage;
-        pullTable();
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
 
-        AnchorPane root=  generateBasicImage();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+//        AnchorPane root = createSimpleImage();
 
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-    public void pullTable() {
-        grains = new Grain[100][100];
-        for (int i=0; i<100; i++) {
-            for (int j=0; j<100; j++) {
-                grains[i][j] = new Grain(i,j, Color.GRAY);
+    public static AnchorPane createSimpleImage() {
+
+        Random r = new Random(255);
+        AnchorPane root = new AnchorPane();
+
+
+        grains = new Grain[500][500];
+        for (int i=0; i<500; i++) {
+            for (int j=0; j<500; j++) {
+                grains[i][j] = new Grain(r.nextInt(255), r.nextInt(255), Color.rgb(r.nextInt(255),r.nextInt(255),r.nextInt(255)));
+                AnchorPane anchorPane = colorPixels(i,j);
+                root.getChildren().add(anchorPane);
 
             }
         }
 
-//        Arrays.asList(grains).forEach(e -> System.out.println(e));
+        return root;
 
     }
 
-    public AnchorPane generateBasicImage() {
+    public static AnchorPane colorPixels(int i, int j) {
 
-        AnchorPane image = new AnchorPane();
-        for (int i=0; i<100; i++) {
-            for (int j=0; j<100; j++) {
-                int x = new Random().nextInt(255);
-                int y = new Random().nextInt(255);
-                int z = new Random().nextInt(255);
-                grains[i][j].setColor(Color.rgb(x,y,z));
-                AnchorPane anchorPane = grains[i][j].createGrainImage();
-                anchorPane.setPrefSize(500,500);
-                anchorPane.setMaxSize(500,500);
+        AnchorPane anchorPane = grains[i][j].createGrainImage();
+        anchorPane.setPrefSize(size,size);
+        anchorPane.setMaxSize(size,size);
 
-                AnchorPane.setTopAnchor(anchorPane, j*500*1.0);
-                AnchorPane.setLeftAnchor(anchorPane, i*500*1.0);
+        AnchorPane.setTopAnchor(anchorPane, j*size*1.0);
+        AnchorPane.setLeftAnchor(anchorPane, i*size*1.0);
 
-                image.getChildren().add(anchorPane);
+        return anchorPane;
 
-            }
-        }
-
-        return image;
     }
-
 
     public static void main(String[] args) {
         launch(args);
